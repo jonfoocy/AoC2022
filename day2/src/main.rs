67e8586c429956a5cfbcf5401c5b0dca93306ea3
Jonathan Foo <jonathanfoocy@gmail.com>
2022-12-05 00:37:39 -0500

    day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,26 +1,10 @@
 #![feature(buf_read_has_data_left)]
 
 use std::{
-    clone,
+    collections::HashMap,
     fs::File,
-    io::{BufRead, BufReader, Read},
+    io::{BufRead, BufReader},
 };
-
-#[derive(Clone, Debug, PartialEq, Eq)]
-enum RPSValue {
-    Rock,
-    Paper,
-    Scissors,
-    Gun,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-enum RPSResult {
-    Win,
-    Draw,
-    Loss,
-    Invalid,
-}
 
 fn get_lines() -> BufReader<File> {
     let f: File = File::open("input.txt").unwrap();
@@ -29,87 +13,65 @@ fn get_lines() -> BufReader<File> {
     reader
 }
 
-fn get_rps_value(c: char) -> RPSValue {
-    match c {
-        'A' | 'X' => RPSValue::Rock,
-        'B' | 'Y' => RPSValue::Paper,
-        'C' | 'Z' => RPSValue::Scissors,
-        _ => RPSValue::Gun,
-    }
+fn initialise_score_map() -> HashMap<String, i32> {
+    let mut score_map: HashMap<String, i32> = HashMap::new();
+
+    // Rock
+    score_map.insert("A X".to_string(), 4); // rock, draw
+    score_map.insert("A Y".to_string(), 8); // paper, win
+    score_map.insert("A Z".to_string(), 3); // scissors, lose
+
+    // Paper
+    score_map.insert("B X".to_string(), 1); // rock, lose
+    score_map.insert("B Y".to_string(), 5); // paper, draw
+    score_map.insert("B Z".to_string(), 9); // scissors, win
+
+    // Scissors
+    score_map.insert("C X".to_string(), 7); // rock, win
+    score_map.insert("C Y".to_string(), 2); // paper, lose
+    score_map.insert("C Z".to_string(), 6); // scissors, draw
+
+    score_map
 }
 
-fn get_result(opp: RPSValue, me: RPSValue) -> RPSResult {
-    if opp == me {
-        RPSResult::Draw
-    } else {
-        if opp == RPSValue::Rock {
-            if me == RPSValue::Paper {
-                RPSResult::Win
-            } else {
-                RPSResult::Loss
-            }
-        } else if opp == RPSValue::Paper {
-            if me == RPSValue::Rock {
-                RPSResult::Loss
-            } else {
-                RPSResult::Win
-            }
-        } else if opp == RPSValue::Scissors {
-            if me == RPSValue::Rock {
-                RPSResult::Win
-            } else {
-                RPSResult::Loss
-            }
-        } else {
-            RPSResult::Invalid
-        }
-    }
-}
+fn initialise_strategy_map() -> HashMap<String, i32> {
+    let mut strategy_map: HashMap<String, i32> = HashMap::new();
 
-fn get_score(me: RPSValue, result: RPSResult) -> i32 {
-    let mut score: i32 = 0;
-    if me == RPSValue::Rock {
-        score += 1;
-    } else if me == RPSValue::Paper {
-        score += 2;
-    } else {
-        score += 3;
-    }
+    // Rock
+    strategy_map.insert("A X".to_string(), 3); // lose: scissors
+    strategy_map.insert("A Y".to_string(), 4); // draw: rock
+    strategy_map.insert("A Z".to_string(), 8); // win: paper
 
-    if result == RPSResult::Win {
-        score += 6;
-    } else if result == RPSResult::Draw {
-        score += 3;
-    } else {
-        score += 0;
-    }
+    // Paper
+    strategy_map.insert("B X".to_string(), 1); // lose: rock
+    strategy_map.insert("B Y".to_string(), 5); // draw: paper
+    strategy_map.insert("B Z".to_string(), 9); // win: scissors
 
-    score
+    // Scissors
+    strategy_map.insert("C X".to_string(), 2); // lose: paper
+    strategy_map.insert("C Y".to_string(), 6); // draw: scissors
+    strategy_map.insert("C Z".to_string(), 7); // win: rock
+
+    strategy_map
 }
 
 fn main() {
     let mut reader = get_lines();
-    let mut totalScore = 0;
+    let mut total_score = 0;
+    let mut total_strategy_score = 0;
+    let score_map: HashMap<String, i32> = initialise_score_map();
+    let strategy_map: HashMap<String, i32> = initialise_strategy_map();
     while reader.has_data_left().unwrap() {
         let mut line: String = String::new();
         reader.read_line(&mut line).unwrap();
+        line = line.trim().to_string();
 
-        let opp = get_rps_value(line.chars().nth(0).unwrap());
-        let me = get_rps_value(line.chars().nth(2).unwrap());
-        let me_clone = me.clone();
-        // println!("opp: {:?}, me: {:?}", opp, me);
+        let score: i32 = *score_map.get(&line).unwrap();
+        total_score += score;
 
-        let result = get_result(opp, me);
-        // println!("result: {:?}", result);
-        let score = get_score(me_clone, result);
-        // println!("score: {}", score);
-        totalScore += score;
-
-        // let println!("{line}");
-        // count += 1;
-        // if count == 2 {
-        //     break;
-        // }
+        let strategy_score: i32 = *strategy_map.get(&line).unwrap();
+        total_strategy_score += strategy_score;
     }
-    println!("total score is: {}", totalScore);
+    println!("total score is: {}", total_score); // part 1
+    println!("total strategy score is: {}", total_strategy_score); // part 2
 }
